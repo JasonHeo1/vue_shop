2ce927d5d110c359e258fb5077e3ce73a1db9a1e
your_name <your_email@example.com>
2021-02-14 10:19:00 +0900

    login complete

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,20 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/standard'
+    '@vue/standard',
+     "eslint:recommended",
+    // "eslint-config-prettier"
   ],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'quotes': ['off', 'single'],
+    'semi': 0,
+    "space-before-function-paren": 0
+    // "semi": [1, "always"]
+    // "no-extra-semi": "error",
   }
 }
