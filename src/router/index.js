4343d4079cedddd7e527d568d07e150e35457c70
your_name <your_email@example.com>
2021-02-14 22:18:34 +0900

    complete menu-bar

--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,51 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Login from "../components/Login.vue";
-import Home from "../components/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    redirect: "/login"
+    path: '/',
+    redirect: '/login'
   },
   {
-    path: "/login",
-    name: "login",
+    path: '/login',
+    name: 'login',
     component: Login
   },
   {
-    path: "/home",
-    name: "home",
-    component: Home
+    path: '/home',
+    name: 'home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      {
+        path: '/welcome',
+        component: Welcome
+      },
+      {
+        path: '/users',
+        component: Users
+      }
+    ]
   }
-];
+]
 
 const router = new VueRouter({
   routes
-});
+})
 
 router.beforeEach((to, from, next) => {
   // to and from are both route objects. must call `next`.
-  if (to.path === "/login") return next();
+  if (to.path === '/login') return next()
   // get token
-  const tokenStr = window.sessionStorage.getItem("token");
-  if (!tokenStr) return next("/login");
-  next();
-});
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
 
-export default router;
+export default router
